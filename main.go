8708821c771418ec92190c370c7967da2214460b
mamoss-oss <a@b.c>
2023-09-09 20:20:58 +0200

    stage2

--- main.go
+++ main.go
@@ -1,24 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-// type rootHandle struct{}
+type apiConfig struct {
+	fileserverHits int
+}
 
-// func (rootHandle) ServeHTTP(http.ResponseWriter, *http.Request) {}
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(200)
+}
+
+func (cfg *apiConfig) displayMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
 
 func main() {
+	conf := apiConfig{fileserverHits: 0}
 	// create new mux
 	mux := http.NewServeMux()
 	// create fileserve http handler at root /
 	fs := http.FileServer(http.Dir("./"))
+
+	// Middle ware wrapping done extensive. Can be made much more compact.
+	// StripPrefix is a built in middleware. It takes the handler, does something, returns the handler.
+	fs_wrapped_one := http.StripPrefix("/app/", fs)
+	// takes the handler, does something, returns the handler
+	fs_wrapped_two := conf.middlewareMetricsInc(fs_wrapped_one)
+
+	// You can either use Handle or HandleFunc. The result is basically the same.
+	// It all depends if you got a http.handler or a func that matches the
+	// implementation of a handlefunc
+
 	//register handler with mux
-	mux.Handle("/app/", http.StripPrefix("/app/", fs))
+	mux.Handle("/app/", fs_wrapped_two)
 
 	// register healthz function with the mux
 	mux.HandleFunc("/healthz", healthz)
+
+	mux.HandleFunc("/metrics", conf.displayMetrics)
+
+	mux.HandleFunc("/reset", conf.resetMetrics)
 
 	// add middleware to mux
 	corsMux := middlewareCors(mux)
---
+++ notes.md
@@ -0,0 +1,20 @@
+# Middleware examples
+
+```go
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+```
+
+
+```go
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+```
